ca77a3987a9701d867bfe70ba37b1f3368e12f7b
Ismael <ismael-fernandez@outlook.es>
2023-11-12 19:04:06 +0100

    Pequeños cambios al estilo

--- controladors/controlador_productes.php
+++ controladors/controlador_productes.php
@@ -38,10 +38,15 @@ elseif ($a === "productedetall")
     $html = array();
     foreach($productes as $fila)
     {
-        $html_element = '<div class="producte-container">
+        $html_element = '<div class="producte-detall-container">
         <h3 class="titol-producte">' . $fila['titulo'] . '</h3>
         <img class="imatge-producte" src="/img/'. $fila['img'] . '" alt="">
+        <div class="producte-descripcio">
+        <p class="descripcio">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum odio assumenda facilis praesentium incidunt. Nam labore laboriosam, ut sit voluptas, vitae deleniti atque expedita placeat aut mollitia veniam repudiandae excepturi.
+        Provident dolores alias accusantium aperiam repudiandae dolor quis voluptatibus. Autem veritatis ullam suscipit sit voluptatum aspernatur vero beatae possimus sequi eligendi, vel totam consequuntur excepturi ratione veniam harum dignissimos illo.</p>
+        </div>
         <p class="preu-producte">' . $fila['precio'] . '€</p>
+        <button class="boto-afegir-carrito">Afegir al cabàs</button>
         </div>';
         array_push($html, $html_element);
     }
